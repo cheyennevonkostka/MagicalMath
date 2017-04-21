37e352185dacac496f88b9837d97b425ecc70b7e
cheyenne <cheyennevonkostka@mycwi.cc>
2017-04-21 09:24:40 -0600

    Friday morning 4.21.17

--- js/math.js
+++ js/math.js
@@ -1,4 +1,46 @@
-// JavaScript Document - just bare bones for testing right now
+// JavaScript Document 
+// 4/20/17
+//Pablo Lomeli: I've commented out almost every single thing on this javascript file and almost completely single handedly cooked this up.
+ 
+// Timer functions
+function settime1(){
+ document.getElementById('timer').innerHTML =
+  "01" + ":" + "00";
+  document.getElementById("totalGiven").innerHTML = 20;
+}
+
+function settime5(){
+ document.getElementById('timer').innerHTML =
+  "05" + ":" + "00";
+  document.getElementById("totalGiven").innerHTML = 100;
+}	
+
+function checkSecond(sec) {
+	if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+	if (sec < 0) {sec = "59"};
+  return sec;
+}
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0)
+  	{
+		results();
+	  	return; 
+	}
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+// stop timer function needed.
+
+
 
 // insert function that grabs the number selected from the list of numbers
 function insertData(){
@@ -19,16 +61,28 @@ function insertData(){
 		if (radios2[i].checked) {
 			// do whatever you want with the checked radio
 			document.getElementById("mathSign").innerHTML = radios2[i].value;
-	
+			document.getElementById("mathSignHIDDEN").innerHTML = radios2[i].value;
 			// only one radio can be logically checked, don't check the rest
 			break;
 		}
+	}
+	
+	//sets the game menu's selected symbols
+	var currentMathSign = document.getElementById("mathSign").innerHTML;
+	if(currentMathSign == "*")
+	{
+		document.getElementById("mathSign").innerHTML = "&times";
+	}
+	if(currentMathSign == "/")
+	{
+		document.getElementById("mathSign").innerHTML = "&divide";
 	}
 	
 	//Disables menu so test may begin.
 	document.getElementById("submitButton").disabled = true;
 	document.getElementById("testing").disabled = false;
 	
+	//these loops disable all the radio buttons
 	var radios1 = document.getElementsByName('number');
 
 	for (var i=0, iLen=radios1.length; i<iLen; i++) {
@@ -40,32 +94,34 @@ function insertData(){
 	  radios2[i].disabled = true;
 	} 
 	
-	selectedSign = document.getElementById("mathSign").innerHTML;
+	if(currentMathSign != "/")
+	{
+		// Random number generator (Needs refinement most likely)
+		var ranNum = Math.floor((Math.random() * 12) + 1);
+		document.getElementById("randomNumber").value = ranNum;
+	}
 	
-	// Random number generator (Needs refinement most likely)
-	var ranNum = Math.floor((Math.random() * 10) + 1);
-	document.getElementById("randomNumber").value = ranNum;
-	
-	/*
-	if(selectedSign == "&divde")
+	else if(currentMathSign == "/")
 	{
 		var selectedNumber = document.getElementById("chosenNumber").value;
 		function GetRandomNumberBetween(lo, hi) {
 		  return Math.floor(lo + Math.random() * (hi - lo));
 		}
 		
+		//start of a random number generator thats divisible by the number selected
 		Number.prototype.FindClosestNumberThatIsDivisibleBy = function(n) {
 		  return Math.round(this / n) * n; 
 		  //simplify as per Guffa
 		};
 		
 		var r = GetRandomNumberBetween(6, 144);
-		var c = r.FindClosestNumberThatIsDivisibleBy(ranNum);
+		var c = r.FindClosestNumberThatIsDivisibleBy(selectedNumber);
 		document.getElementById("randomNumber").value = c;
 		
 	}
-	*/
 	
+	startTimer();
+	document.getElementById("testing").focus();
 
 }
 
@@ -82,16 +138,79 @@ function onTestChange() {
 		
 		if(testSolution == true){
 			// calls score function to count the answer right.
-			var ranNum = Math.floor((Math.random() * 10) + 1);
-			document.getElementById("randomNumber").value = ranNum;
-			alert("Answer was right and this ran good")
+			currentMathSign = document.getElementById("mathSignHIDDEN").innerHTML;
+			
+			if(currentMathSign != "/")
+			{
+				// Random number generator (Needs refinement most likely)
+				var ranNum = Math.floor((Math.random() * 12) + 1);
+				document.getElementById("randomNumber").value = ranNum;
+				//alert("Answer was right and this ran good")
+				document.getElementById("testing").value = "";
+				var answer = "correct";
+				showResult(answer);
+			}
+			
+			else if(currentMathSign == "/")
+			{
+				//start of a random number generator thats divisible by the number selected
+				var selectedNumber = document.getElementById("chosenNumber").value;
+				function GetRandomNumberBetween(lo, hi) {
+				  return Math.floor(lo + Math.random() * (hi - lo));
+				}
+				
+				Number.prototype.FindClosestNumberThatIsDivisibleBy = function(n) {
+				  return Math.round(this / n) * n; 
+				  //simplify as per Guffa
+				};
+				
+				var r = GetRandomNumberBetween(6, 144);
+				var c = r.FindClosestNumberThatIsDivisibleBy(selectedNumber);
+				document.getElementById("randomNumber").value = c;
+				//alert("Answer was right and this ran good (division")
+				document.getElementById("testing").value = "";
+				var answer = "correct";
+				showResult(answer);
+				
+			}
 		
 		}
 		else if(testSolution == false){
-			//does not call score function to not count cus user got answer wrong.
-			var ranNum = Math.floor((Math.random() * 10) + 1);
-			document.getElementById("randomNumber").value = ranNum;
-			alert("Answer was wrong and this ran good")
+			currentMathSign = document.getElementById("mathSignHIDDEN").innerHTML;
+			
+			if(currentMathSign != "/")
+			{
+				// Random number generator (Needs refinement most likely)
+				var ranNum = Math.floor((Math.random() * 12) + 1);
+				document.getElementById("randomNumber").value = ranNum;
+				//alert("Answer was wrong and this ran good")
+				document.getElementById("testing").value = "";
+				var answer = "wrong";
+				showResult(answer);
+			}
+			
+			else if(currentMathSign == "/")
+			{
+				//start of a random number generator thats divisible by the number selected
+				var selectedNumber = document.getElementById("chosenNumber").value;
+				function GetRandomNumberBetween(lo, hi) {
+				  return Math.floor(lo + Math.random() * (hi - lo));
+				}
+				
+				Number.prototype.FindClosestNumberThatIsDivisibleBy = function(n) {
+				  return Math.round(this / n) * n; 
+				  //simplify as per Guffa
+				};
+				
+				var r = GetRandomNumberBetween(6, 144);
+				var c = r.FindClosestNumberThatIsDivisibleBy(selectedNumber);
+				document.getElementById("randomNumber").value = c;
+				//alert("Answer was wrong and this ran good (division")
+				document.getElementById("testing").value = "";
+				var answer = "wrong";
+				showResult(answer);
+				
+			}		
 		}
 		
 
@@ -101,14 +220,16 @@ function onTestChange() {
 }
 
 function calculationFunction() {
+	// grabs all values to be used for the calculation
 	var userInput = document.getElementById("testing").value;
 	var firstNumber = document.getElementById("randomNumber").value;
 	var secondNumber = document.getElementById("chosenNumber").value;
-	var mathSign = document.getElementById("mathSign").value;
+	var mathSign = document.getElementById("mathSignHIDDEN").innerHTML;
 	var answer = userInput;
 	var mathSignJS = mathSign;
 	parseInt(answer);
 	
+	// 4 if statements to catch what math sign there is to use.
 	if (mathSignJS == "+")
 	{
 		var solution = parseInt(firstNumber) + parseInt(secondNumber);
@@ -130,7 +251,7 @@ function calculationFunction() {
 	
 	if (mathSignJS == "*")
 	{
-		alert("made it this far")
+		
 		var solution = parseInt(firstNumber) * parseInt(secondNumber);
 		if(answer == solution)
 		{
@@ -140,7 +261,7 @@ function calculationFunction() {
 	
 	if (mathSignJS == "/")
 	{
-		alert("you got this far")
+		
 		var solution = parseInt(firstNumber) / parseInt(secondNumber);
 		if(answer == solution)
 		{
@@ -153,4 +274,80 @@ function calculationFunction() {
 	
 }
 
-/// or gives you a wrong message and updates the test question/ Calculation function contains the method for testing if answer is right, then calls the message function that either updates the test question
+//counter feature coding below
+var rightcounter = 0;
+var counter = 0;
+var typeTest = document.getElementById("totalGiven").innerHTML;
+
+function showResult(answerInput) {
+	
+	var delayMillis = 1000; //1 second
+
+	if(answerInput == "correct")
+	{
+		rightcounter++;
+		counter++;
+		document.getElementById("answerNotification").innerHTML = "Good job!";
+		
+		setTimeout(function() {
+	  	document.getElementById("answerNotification").innerHTML = "";
+		}, delayMillis);
+		
+		document.getElementById("currentAnswered").innerHTML = rightcounter;
+		
+	}
+	
+	if(answerInput == "wrong")
+	{
+		counter++;
+		document.getElementById("answerNotification").innerHTML = "Wrong!";
+		
+		setTimeout(function() {
+	  	document.getElementById("answerNotification").innerHTML = "";
+		}, delayMillis);
+		
+	}
+	
+	if(counter == 20 && typeTest == "20")
+	{
+		document.getElementById("testing").disabled = true;
+		document.getElementById("randomNumber").value = "";
+		document.getElementById("chosenNumber").value = "";
+		
+		results();
+	}
+	if(counter == 100 && typeTest == "100")
+	{
+		document.getElementById("testing").disabled = true;
+		document.getElementById("randomNumber").value = "";
+		document.getElementById("chosenNumber").value = "";
+		
+		results();
+	}
+	// if statement need to catch when the timer goes off to do what these ones will do as well
+	
+	
+}
+
+
+// results feature coded below
+function results()
+{
+		
+	if(typeTest == "20")
+	{
+		var results = rightcounter / 20;
+		results = results * 100;
+		document.getElementById("testResults").innerHTML = results.toFixed(2) + "%";
+		
+	}
+	if(typeTest == "50")
+	{
+		var results = rightcounter / 100;
+		results = results * 100;
+		document.getElementById("testResults").innerHTML = results.toFixed(2) + "%";
+		
+	}
+}
+
+
